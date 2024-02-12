f627f6de2e68ce21fc1ed4f15350ea7e7b76aea7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-02-12 17:09:57 +0100

    switch route with parameter works

--- src/app/components/profilo/profilo.component.ts
+++ src/app/components/profilo/profilo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-profilo',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./profilo.component.scss']
 })
 export class ProfiloComponent {
+  constructor(private route:ActivatedRoute,
+    private toastr:ToastrService,private router:Router){}
 
+      ngOnInit(): void {
+
+        if(localStorage.getItem('user')){
+
+        }
+        this.route.params.subscribe(params => {
+          console.log(params['id'])
+        })
+      }
 }
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-task',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./task.component.scss']
 })
 export class TaskComponent {
+  constructor(private route:ActivatedRoute,
+    private toastr:ToastrService,private router:Router){}
 
+      ngOnInit(): void {
+
+        if(localStorage.getItem('user')){
+
+        }
+        this.route.params.subscribe(params => {
+          console.log(params['id'])
+        })
+      }
 }
